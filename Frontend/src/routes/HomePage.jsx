d092c26ad603a56683b20ce2027603765779c17a
Aman Kumar <aman228115918@gmail.com>
2024-10-09 21:27:45 +0530

    animated homepage done

--- Frontend/src/routes/HomePage.jsx
+++ Frontend/src/routes/HomePage.jsx
@@ -3,8 +3,41 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="text-white">
-      <Link to="/dashboard">Dashboard</Link>
+    <div className="flex items-center gap-24 h-[100%]">
+      {/* left */}
+      <div className=" flex flex-col items-center justify-center gap-4 flex-1 text-center">
+        <h1 className="text-8xl bg-gradient-to-r from-[#217bfe] to-[#e55571] text-transparent bg-clip-text">
+          INTELLEXA
+        </h1>
+        <h2 className="font-bold">
+          Supercharge your creativity and productivity
+        </h2>
+        <h3 className="font-medium max-w-[70%]">
+          An advanced AI-powered chatbot designed to offer dynamic, real-time
+          conversational experiences and intelligent assistance across a wide
+          range of topics.
+        </h3>
+        <Link
+          to="/dashboard"
+          className="pt-4 pb-4 pl-6 pr-6 bg-[#217bfe] rounded-3xl text-sm mt-5 hover:bg-white hover:text-blue-500"
+        >
+          Get Started
+        </Link>
+      </div>
+
+      {/* right */}
+
+      <div className="flex-1 flex items-center justify-center h-[100%]">
+        <div className="flex items-center justify-center bg-[#140e2d] rounded-3xl w-[80%] h-[50%] relative">
+          <div className="w-[100%] h-[100%] overflow-hidden absolute top-0 left-0 rounded-2xl">
+            <div className="bg-bg-image opacity-20 w-[200%] h-[100%] bg-[length:auto_100%] animate-bgAnimate"></div>
+          </div>
+          <img
+            src="https://github.com/safak/chatgpt-clone/blob/completed/client/public/bot.png?raw=true"
+            className="w-[100%] h-[100%] object-contain animate-botAnimate"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        botAnimate: "botAnimate 2s ease-in-out infinite alternate",
+        bgAnimate: "bgAnimate 8s ease-in-out infinite alternate",
+      },
+      keyframes: {
+        botAnimate: {
+          "0%": { transform: "scale(1) rotate(0deg)" },
+          "100%": { transform: "scale(1.1) rotate(-5deg)" },
+        },
+        bgAnimate: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      backgroundImage: {
+        "bg-image":
+          "url('https://github.com/safak/chatgpt-clone/blob/completed/client/public/bg.png?raw=true')",
+      },
+    },
   },
   plugins: [],
-}
+};
