e135de000ea7e1afe1b71f9f359847c885508a38
Aman Kumar <aman228115918@gmail.com>
2024-10-12 22:18:19 +0530

    google gemini integration done

--- Frontend/src/components/NewPrompt.jsx
+++ Frontend/src/components/NewPrompt.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import Upload from "./Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
   const [image, setImage] = useState({
     isLoading: false,
     error: "",
@@ -12,7 +16,21 @@ const NewPrompt = () => {
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behaviour: "smooth" });
-  }, []);
+  }, [question, answer, image.dbData]);
+
+  const add = async (text) => {
+    setQuestion(text);
+    const result = await model.generateContent(text);
+    setAnswer(result.response.text());
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text);
+    e.target.text.value = "";
+  };
 
   return (
     <div>
@@ -22,17 +40,35 @@ const NewPrompt = () => {
           urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
           path={image.dbData?.filePath}
           width="380"
-          transformation={[{width:380}]}
+          transformation={[{ width: 380 }]}
         />
       )}
+
+      {question && (
+        <div className="flex flex-col">
+          <div className="p-4 bg-[#2c2937] rounded-lg max-w-[80%] self-end">
+            {question}
+          </div>
+        </div>
+      )}
+      {answer && (
+        <div className="p-4 ">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div className="pb-[100px]" ref={endRef}></div>
-      <form className="w-[80%] absolute bottom-0 bg-[#2c2937] rounded-[20px] flex items-center gap-5 px-5">
+
+      <form
+        className="w-[80%] absolute bottom-0 bg-[#2c2937] rounded-[20px] flex items-center gap-5 px-5"
+        onSubmit={handleSubmit}
+      >
         <Upload setImage={setImage} />
         <input id="file" type="file" multiple={false} hidden />
         <input
           type="text"
-          placeholder="Ask me anythimg..."
+          placeholder="Ask me anything..."
           className="flex-1 p-5 border-none outline-none bg-transparent text-[#ececec]"
+          name="text"
         />
         <button className="rounded-full bg-[#605e68] border-none p-2 flex items-center justify-center ">
           <img
--- Frontend/src/routes/ChatPage.jsx
+++ Frontend/src/routes/ChatPage.jsx
@@ -5,7 +5,7 @@ const ChatPage = () => {
   return (
     <div className="h-[100%] flex flex-col items-center relative">
       <div className="flex-1 overflow-y-auto overflow-x-hidden w-[100%] flex justify-center">
-        <div className="w-[80%] flex flex-col">
+        <div className="w-[80%] flex flex-col gap-5">
           <div className="p-4">Test message from ai</div>
           <div className="p-4 bg-[#2c2937] rounded-lg max-w-[80%] self-end">
             Test message from user
